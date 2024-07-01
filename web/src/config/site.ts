741e7be161ae0dd8142886d9002c82b45f84d8d4
Tejas Nayak B <tejasnayak1@gmail.com>
2024-07-01 12:28:16 +0530

    Initial Commit

---
+++ web/src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "LeafDoc",
+  url: "https://LeafDoc.vercel.app",
+  ogImage: "https://LeafDoc.vercel.app/og.jpg",
+  description:
+    "Your digital gateway to the botanical world. Unearth a wealth of insights, from species details to disease detection, effortlessly bridging the gap between technology and the beauty of nature.",
+  links: {
+    twitter: "",
+    github: "https://github.com/tejasnayak1/LeafDoc",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
